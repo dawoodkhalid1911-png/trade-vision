b07e7c9fc751605b39efff3dd845b32cfb2e324a
Dawood Khalid <dawoodkhalid1911@gmail.com>
2026-05-18 10:02:13 -0700

    Add config.js frontend config

---
+++ frontend/src/config.js
@@ -0,0 +1,9 @@
+// Config file for PR 3
+const APP_CONFIG = {
+  name: 'Trade Vision',
+  version: '1.0.0',
+  apiUrl: 'http://localhost:5000/api',
+  defaultCity: 'Lahore'
+};
+
+export default APP_CONFIG;
